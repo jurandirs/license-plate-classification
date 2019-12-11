7432f58a4692bf31d08cf031dd113b0fdea0e857
Jurandir Silva <jurandirjdsilva@gmail.com>
2019-12-11 15:57:31 -0300

    Update

--- prepare_alpr.sh
+++ prepare_alpr.sh
@@ -1,5 +1,8 @@
-sudo apt install node npm -y
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt install -y nodejs
+node -v
+sudo apt install npm
 sudo npm install
-sudo npm npm cache clean
-sudo sudo npm install -g npm
+sudo npm cache clean
+sudo npm install -g npm
 sudo npm install pi-camera
