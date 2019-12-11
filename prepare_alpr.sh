91cce0e7345b34acfbb26c703eb88a49a720328f
Jurandir Silva <jurandirjdsilva@gmail.com>
2019-12-11 17:01:12 -0300

    Update

--- prepare_alpr.sh
+++ prepare_alpr.sh
@@ -3,4 +3,10 @@ tar -xzf node-v9.7.1-linux-armv6l.tar.gz
 sudo cp -r node-v9.7.1-linux-armv6l/* /usr/local/
 node -v
 npm -v
+npm init
 sudo npm install pi-camera
+
+# https://raspberrypi.stackexchange.com/questions/13764/what-causes-enospc-error-when-using-the-raspberry-pi-camera-module
+cp /boot/cmdline.txt /boot/cmdline.txt~
+echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait bcm2708.w1_gpio_pin=18" > /boot/cmdline.txt
+
