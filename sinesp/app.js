5e2d70397921882674c9570b40870bef149923db
Denis Masunaga <denismasunaga@gmail.com>
2019-12-14 20:27:25 -0300

    bot implementado

--- sinesp/app.js
+++ sinesp/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-
+const PORT = process.env.PORT || 5000;
+const telegraToken = '715749096:AAEnlGKIjCfU8mKZd7ILNp2PUQC8vyNfEu8';
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(telegraToken, {polling: true});
 const placaOld = new RegExp("[a-zA-Z]{3}[0-9]{4}");
 const placaMercosul = new RegExp("[a-zA-Z]{3}[0-9]{1}[a-zA-Z]{1}[0-9]{2}");
+
+let chatIds = [];
 
 app.get('/placa/:placa', function (req, res) {
     // console.log(req.params.placa);
@@ -15,12 +20,15 @@ function verificaPlaca(placa) {
 
     try {
         if (placaOld.test(placa) || placaMercosul.test(placa)) {
-            if(getRandomInt() == 0) {
+            if(numeroRandom() == 0) {
+                notificaSituacao(`Placa ${placa} - ok`);
                 return {situacao: 'Sem restrição'}
             } else {
+                notificaSituacao(`Placa ${placa} - !!! FURTO !!!`);
                 return {situacao: 'Furto'}
             }
         } else {
+            notificaSituacao('Não foi possível ler a placa!');
             return {result: 'Placa inválida'}
         }
     } catch (err) {
@@ -28,9 +36,21 @@ function verificaPlaca(placa) {
     }
 }
 
-function getRandomInt() {
+function numeroRandom() {
     return Math.floor(Math.random() * Math.floor(2));
 }
 
+function notificaSituacao(msg) {
+    chatIds.forEach(function(chat) {
+        bot.sendMessage(chat, msg);
+    });
+}
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    chatIds.push(chatId);
+    bot.sendMessage(chatId, 'Cadastrei você!');
+});
+
 // console.log(verificaPlaca('AASD1C22'));
-app.listen(80);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
